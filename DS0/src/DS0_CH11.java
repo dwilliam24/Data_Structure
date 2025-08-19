9cd174d48fae679f65d7fe033d53a215523382f4
dwilliam24 <k2306075@students.katyisd.org>
2025-08-19 09:44:34 -0500

    .

--- DS0/src/DS0_CH11.java
+++ DS0/src/DS0_CH11.java
@@ -2,20 +2,20 @@ public class DS0_CH11 {
 
     public static boolean isSolvable(char[][] maze, boolean[][] beenThere, int column, int row){
         boolean x =false;
-        beenThere[column][row]=true;
-        if (maze[column][row]=='E') return true;
+        beenThere[row][column]=true;
+        if (maze[row][column]=='E') return true;
 
-        if (column+1<maze.length&&!beenThere[column+1][row]&&maze[column+1][row]!='W'){
-            x= isSolvable(maze,beenThere,column+1,row);
+        if (row+1<maze.length&&!beenThere[row+1][column]&&maze[row+1][column]!='W'){
+            x= isSolvable(maze,beenThere,column,row+1);
         }
-        if(column-1>=0&&!beenThere[column-1][row]&&maze[column-1][row]!='W'&&!x){
-            x= isSolvable(maze,beenThere,column-1,row);
-        }
-        if(row-1>=0&&!beenThere[column][row-1]&&maze[column][row-1]!='W'&&!x){
+        if(row-1>=0&&!beenThere[row-1][column]&&maze[row-1][column]!='W'&&!x){
             x= isSolvable(maze,beenThere,column,row-1);
         }
-        if(row+1<maze[0].length&&!beenThere[column][row+1]&&maze[column][row+1]!='W'&&!x){
-            x= isSolvable(maze,beenThere,column,row+1);
+        if(column-1>=0&&!beenThere[row][column-1]&&maze[row][column-1]!='W'&&!x){
+            x= isSolvable(maze,beenThere,column-1,row);
+        }
+        if(column+1<maze[0].length&&!beenThere[row][column+1]&&maze[row][column+1]!='W'&&!x){
+            x= isSolvable(maze,beenThere,column+1,row);
         }
          return x;
     }
---
+++ DS0/src/DS0_CH12.java
@@ -0,0 +1,3 @@
+public class DS0_CH12 {
+    
+}
