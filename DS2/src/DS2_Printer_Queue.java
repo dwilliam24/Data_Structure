899f848309e8e8055d76b4f87fb79a3b0d36f5fc
dwilliam24 <k2306075@students.katyisd.org>
2025-09-15 09:52:21 -0500

    .

--- DS2/src/DS2_Printer_Queue.java
+++ DS2/src/DS2_Printer_Queue.java
@@ -1,7 +1,13 @@
 import java.io.File;
+import java.util.Scanner;
 
 public class DS2_Printer_Queue {
     public static void main(String[] args){
-        File file = new File()
+        
+
+        Scanner e = new Scanner(System.in);
+        File file = new File(e.nextLine());
+        e.close();
+
     }
 }
