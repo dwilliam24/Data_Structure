bbc44f38870ade12b0263b17b42be87fb6ad2a86
dwilliam24 <k2306075@students.katyisd.org>
2025-09-17 09:01:37 -0500

    .

--- DS2/src/DS2_Printer_Queue.java
+++ DS2/src/DS2_Printer_Queue.java
@@ -1,13 +1,38 @@
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class DS2_Printer_Queue {
     public static void main(String[] args){
-        
-
+        MyQueue<Job> jobs = new MyQueue<>();
+        System.out.print("Enter job file name: ");
         Scanner e = new Scanner(System.in);
         File file = new File(e.nextLine());
         e.close();
+
+        try{
+            Scanner fileScanner= new Scanner(file);
+            int number = 0;
+            while (fileScanner.hasNextLine()) {
+                number++;
+                String a = fileScanner.nextLine();
+                jobs.offer(new Job(number, Integer.parseInt(a.split(",")[0]),Integer.parseInt(a.split(",")[1])));
+            }
+            fileScanner.close();
+        }
+        catch (Exception o){
+            o.printStackTrace();
+        }
+
+        while (!jobs.isEmpty()){
+            int time =0;
+            
+            Job temp = jobs.poll();
+            System.out.println("Time "+temp.getPrintStart()+"s: Job #"+temp.getJobNumber()+" Received ("+temp.getPages()+" pages)");
+            System.out.println("Time "+temp.getBufferingStart()+"s: Job #"+temp.getJobNumber()+" Buffering Started");
+            System.out.println("Time "+temp.getBufferingEnd()+"s: Job #"+temp.getJobNumber()+" Finished Buffering and Started Printing");
+            System.out.println("Time "+temp.getPrintEnd()+"s: Job #"+temp.getJobNumber()+" Finished Printing\n");
+        }
 
     }
 }
