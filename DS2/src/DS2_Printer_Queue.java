b69f8c961c9a89ca673e86c58d27cd0312137369
dwilliam24 <k2306075@students.katyisd.org>
2025-09-17 09:55:17 -0500

    .

--- DS2/src/DS2_Printer_Queue.java
+++ DS2/src/DS2_Printer_Queue.java
@@ -23,16 +23,40 @@ public class DS2_Printer_Queue {
         catch (Exception o){
             o.printStackTrace();
         }
-
+        int jobSize = jobs.size();
+        int totalPages = 0;
+        int time=0;
+        int idle =0;
+        double jobTime =0;
         while (!jobs.isEmpty()){
-            int time =0;
-            
+
             Job temp = jobs.poll();
-            System.out.println("Time "+temp.getPrintStart()+"s: Job #"+temp.getJobNumber()+" Received ("+temp.getPages()+" pages)");
+
+            totalPages+=temp.getPages();
+            if (time<temp.getSubmissionTime())
+            {
+                idle += temp.getSubmissionTime()-time;
+                time = temp.getSubmissionTime();
+            }
+
+
+            temp.setPrintStart(temp.getSubmissionTime());
+            System.out.println("\nTime "+temp.getPrintStart()+"s: Job #"+temp.getJobNumber()+" Received ("+temp.getPages()+" pages)");
+            temp.setBufferingStart(time);
+            temp.setBufferingEnd(temp.getBufferingStart()+3);
             System.out.println("Time "+temp.getBufferingStart()+"s: Job #"+temp.getJobNumber()+" Buffering Started");
             System.out.println("Time "+temp.getBufferingEnd()+"s: Job #"+temp.getJobNumber()+" Finished Buffering and Started Printing");
-            System.out.println("Time "+temp.getPrintEnd()+"s: Job #"+temp.getJobNumber()+" Finished Printing\n");
+            temp.setPrintEnd(temp.getBufferingEnd()+(5*temp.getPages()));
+            System.out.println("Time "+temp.getPrintEnd()+"s: Job #"+temp.getJobNumber()+" Finished Printing");
+            time= temp.getPrintEnd();
+
+            jobTime+=temp.getBufferingEnd()-temp.getPrintStart();
         }
+        System.out.println("\nPrinting Simulation Complete.");
+        System.out.println("Total Print Jobs: "+jobSize);
+        System.out.println("Total Pages: "+ totalPages);
+        System.out.printf("Average Job Wait Time: %1.1fs\n",jobTime/jobSize);
+        System.out.println("Idle Time: "+idle+"s");
 
     }
 }
