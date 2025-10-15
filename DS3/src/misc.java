b6d496c1a999d8e1d5fe0c0bce7dbe22059c2a08
dwilliam24 <k2306075@students.katyisd.org>
2025-10-15 09:55:34 -0500

    .

--- DS3/src/misc.java
+++ DS3/src/misc.java
@@ -3,14 +3,16 @@ import java.util.*;
 public class misc {
     public static void main(String[] args)
     {
-        Map<String,Integer> map= new TreeMap<String,Integer>();
+        Map<String,Set> map= new TreeMap<String,Set>();
+        Set<Integer> t = new HashSet<>();
+        t.add(3);
+        t.add(56);
+        map.put("Z",t);
+        map.put("Y",t);
+        map.put("X",t);
+        map.put("Y",t);
 
-        map.put("Z",2);
-        map.put("Y",3);
-        map.put("X",4);
-        map.put("Y",1);
-
-        Iterator<String> it = map.keySet().iterator();
+        Iterator it = map.values().iterator();
 
         while(it.hasNext())
             System.out.print(it.next() + " ");
