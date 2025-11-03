4d2bbae96602e2635cda0549ba1968968991787e
dwilliam24 <k2306075@students.katyisd.org>
2025-11-03 09:02:15 -0600

    .

--- DS4/src/DS4_HashTable.java
+++ DS4/src/DS4_HashTable.java
@@ -2,43 +2,63 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public class DS4_HashTable<K,V> implements DS4_HashTable_Interface<K,V> {
-    private ArrayList<ArrayList<DS4_Entry<K,V>>> Hashtable;
+    private ArrayList<ArrayList<DS4_Entry<K,V>>> hashtable;
+    private int loadFactor;
+    private int tombStones;
     public DS4_HashTable(int bucketCapacity, int loadFactor, int tableSize)
     {
-        Hashtable = new ArrayList<>(tableSize);
-        for (int i = 0; i < tableSize; i++) {
-
+        hashtable = new ArrayList<>(tableSize);
+        this.loadFactor=loadFactor;
+        for (int i = 0; i < hashtable.size(); i++) {
+            hashtable.set(i, new ArrayList<>(bucketCapacity));
         }
-
     }
 
-
-    private int hash(K key){
-        return 0;
-    }
 
     @Override
     public void clear() {
-
+        for (int i = 0; i < hashtable.size(); i++) {
+            for (int j = 0; j < hashtable.get(i).size(); j++) {
+                hashtable.get(i).set(j, null);
+            }
+        }
     }
 
     @Override
     public int size() {
-        return 0;
+        int size =0;
+        for (int i = 0; i < hashtable.size(); i++) {
+            for (int j = 0; j < hashtable.get(i).size(); j++) {
+                if (hashtable.get(i).get(j) != null){
+                    size++;
+                }
+            }
+        }
+        return size;
     }
 
     @Override
     public int tombstones() {
-        return 0;
+        return tombStones;
     }
 
     @Override
     public boolean contains(K key) {
+        for (int i = 0; i < hashtable.size(); i++) {
+            for (int j = 0; j < hashtable.get(i).size(); j++) {
+                if (hashtable.get(i).get(j).equals(key)){
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
     @Override
     public V insert(K key, V value) {
+        if (!hashtable.get(hash(key)).contains(null)){
+
+        }
         return null;
     }
 
@@ -47,10 +67,12 @@ public class DS4_HashTable<K,V> implements DS4_HashTable_Interface<K,V> {
         return null;
     }
     public int tableSize(){
-        return 0;
+        return hashtable.size();
     }
-    public Iterator<K> iterator()
-    {
-       return null;
+    public Iterator<K> iterator() {
+        return null;
+    }
+    private int hash(K key){
+        return key.hashCode()%tableSize();
     }
 }
