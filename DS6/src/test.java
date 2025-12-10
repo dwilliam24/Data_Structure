432a44ba1659d2ea23c97ec78c59cb1063876321
dwilliam24 <k2306075@students.katyisd.org>
2025-12-10 09:03:28 -0600

    .

---
+++ DS6/src/test.java
@@ -0,0 +1,14 @@
+public class test {
+    public static void main(String[] args){
+        MaxHeap<Integer> heap= new MaxHeap<>();
+
+        heap.insert(5);
+        heap.insert(8);
+        heap.insert(3);
+        heap.insert(2);
+        heap.insert(7);
+        heap.insert(-1);
+
+        System.out.println(heap);
+    }
+}
