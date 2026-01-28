0ea81ccfaf2ac9ffb445552271368590d81f4daf
dwilliam24 <k2306075@students.katyisd.org>
2026-01-28 10:25:11 -0600

    .

--- DS8/src/DS8_Dijkstras.java
+++ DS8/src/DS8_Dijkstras.java
@@ -4,6 +4,7 @@ import java.util.Collections;
 
 public class DS8_Dijkstras {
     public static int dijkstras_Weighted(String[] edges, String vertices, char start, char end) {
+        System.out.println("start:");
         ArrayList<DS8_Weighted_Node> list = new ArrayList<DS8_Weighted_Node>();
         list.add(new DS8_Weighted_Node(start , 0));
 
@@ -15,15 +16,37 @@ public class DS8_Dijkstras {
         for (int i = 0; i < list.size(); i++) {
             sorted.add(list.get(i));
         }
+        System.out.println(sorted);
         Collections.sort(sorted);
+        System.out.println(sorted+"\n\n\n");
         while (!sorted.isEmpty()){
             DS8_Weighted_Node temp = sorted.remove(0);
-            if (temp.getLocation()==end) return temp.getDistance();
+            System.out.println("\n");
+            System.out.println(temp.getLocation());
+            if (temp.getLocation()==end&&temp.getDistance()!=Integer.MAX_VALUE) return temp.getDistance();
             if (temp.getDistance()==Integer.MAX_VALUE) break;
             for (int i = 0; i < edges.length; i++) {
-                String t = edges[i].substring(0,1);
-                if (t.contains(temp.getLocation()))
+                String t = edges[i].substring(0,2);
+                System.out.println(t);
+                if (t.substring(0, 1).equals("" + (temp.getLocation()))){
+                    int distance = temp.getDistance()+Integer.parseInt(edges[i].substring(2));
+                    System.out.println(distance);
+                    DS8_Weighted_Node n = null;
+                    for (int a = 0; a<sorted.size(); a++){
+                        if (sorted.get(a).getLocation()==(t.substring(1,2)).toCharArray()[0]){
+                            n=sorted.get(a);
+                        }
+                    }
+                    if (distance<n.getDistance()) {
+                        n.setDistance(distance);
+                        System.out.println("a");
+                    }
+                }
             }
+            System.out.println(sorted);
+            Collections.sort(sorted);
+            System.out.println(sorted+"\n\n\n");
         }
+        return -1;
     }
 }
