5551ec00950180267f860fab455d9da1d3dc5b4b
Di11Pick1es <k2306075@students.katyisd.org>
2026-02-03 23:29:40 -0600

    .

--- DS8/src/DS8_AStar.java
+++ DS8/src/DS8_AStar.java
@@ -1,54 +1,135 @@
 import java.util.ArrayList;
 import java.awt.Point;
+import java.util.Arrays;
 import java.util.Collections;
 
 public class DS8_AStar {
-    public static DS8_Path_Solution aStar_Simple(char[][] maze){
-        ArrayList<DS8_AStar_Node<Point>> open = new ArrayList();
-        ArrayList<DS8_AStar_Node<Point>> closed = new ArrayList<>();
+    public static DS8_Path_Solution aStar_Simple(char[][] maze) {
+
+        int rows = maze.length;
+        int cols = maze[0].length;
+
         Point start = null;
         Point end = null;
-        for (int y = 0; y < maze.length; y++) {
-            for (int x =0 ;x<maze[0].length; x++){
-                if (maze[y][x]=='S') start = new Point(x,y);
-                else if (maze[y][x]=='E') end = new Point(x,y);
+
+        for (int y = 0; y < rows; y++) {
+            for (int x = 0; x < cols; x++) {
+                if (maze[y][x] == 'S') start = new Point(x, y);
+                if (maze[y][x] == 'E') end = new Point(x, y);
             }
         }
-        int h = Math.abs(start.x- end.x)+ Math.abs(start.y- end.y);
-        open.add(new DS8_AStar_Node<>(start,null, 0, h));
 
-        while(!open.isEmpty()){
+        ArrayList<DS8_AStar_Node<Point>> open = new ArrayList<>();
+        ArrayList<Point> closed = new ArrayList<>();
+
+        open.add(new DS8_AStar_Node<>(start, null, 0,
+                Math.abs(start.x - end.x) + Math.abs(start.y - end.y)));
+
+        while (!open.isEmpty()) {
+
             Collections.sort(open);
-            DS8_AStar_Node<Point> temp = open.remove(open.size()-1);
+            DS8_AStar_Node<Point> temp = open.remove(0);
 
-            if (temp.getLocation().equals(end)){
-                return new DS8_Path_Solution(closed, temp.getF());
+            if (temp.getLocation().equals(end)) {
+                ArrayList<Point> path = new ArrayList<>();
+                DS8_AStar_Node<Point> cur = temp;
+
+                while (cur != null) {
+                    path.add(0, cur.getLocation());
+                    cur = cur.getParent();
+                }
+
+                return new DS8_Path_Solution<Point>(path, path.size() - 1);
             }
 
-            int[][] dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-            for (int[] i : dir) {
-                int y = temp.getLocation().y + i[1];
-                int x = temp.getLocation().x + i[0];
-                if (x >= 0 && x < maze[0].length && y >= 0 && y < maze.length) {
-                    DS8_AStar_Node<Point> smallestF = open.get(0);
-                    for (int a = 0; a< open.size(); a++){
-                        if (smallestF.getF()<open.get(a).getF()){
-                            smallestF=open.get(a);
-                        }
-                    }
-                    if (smallestF.getLocation().equals(end)) return new DS8_Path_Solution(closed, temp.getF());
-                    closed.add(smallestF);
-                    for (int[] k : dir) {
-                        y = smallestF.getLocation().y + k[1];
-                        x = smallestF.getLocation().x + k[0];
-                        if (x >= 0 && x < maze[0].length && y >= 0 && y < maze.length) {
-                            
-                        }
+            closed.add(temp.getLocation());
+
+            int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+            for (int[] d : dirs) {
+                int x = temp.getLocation().x + d[0];
+                int y = temp.getLocation().y + d[1];
+
+                if (y < 0 || y >= rows || x < 0 || x >= cols) continue;
+                if (maze[y][x] == 'W') continue;
+
+                Point next = new Point(x, y);
+
+                boolean visited = false;
+                for (Point p : closed) {
+                    if (p.equals(next)) {
+                        visited = true;
+                        break;
                     }
                 }
+                if (visited) continue;
+
+                int g = temp.getG() + 1;
+                int h = Math.abs(x - end.x) + Math.abs(y - end.y);
+
+                open.add(new DS8_AStar_Node<Point>(next, temp, g, h));
+            }
+        }
+        return null;
+    }
+
+
+    public static int aStar_JetPack(char[][] maze) {
+        int rows = maze.length;
+        int cols = maze[0].length;
+
+        Point start = null;
+        Point end = null;
+
+        for (int y = 0; y < rows; y++) {
+            for (int x = 0; x < cols; x++) {
+                if (maze[y][x] == 'S') start = new Point(x, y);
+                if (maze[y][x] == 'E') end = new Point(x, y);
+            }
+        }
+
+        ArrayList<DS8_AStar_Node<Point>> open = new ArrayList<>();
+        ArrayList<Point> closed = new ArrayList<>();
+
+        open.add(new DS8_AStar_Node<>(start, null, 0, 0));
+
+        while (!open.isEmpty()) {
+
+            Collections.sort(open);
+
+            DS8_AStar_Node<Point> temp = open.remove(0);
+
+            if (temp.getLocation().equals(end)) {
+                return temp.getG();
             }
 
+            closed.add(temp.getLocation());
 
+            int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {-1, 1}, {1, -1}, {-1, -1}};
+
+            for (int[] d : dirs) {
+                int x = temp.getLocation().x + d[0];
+                int y = temp.getLocation().y + d[1];
+
+                if (y < 0 || y >= rows || x < 0 || x >= cols) continue;
+
+                Point next = new Point(x, y);
+
+                if (closed.contains(next)) continue;
+
+                int g = 0;
+
+                if (maze[y][x] == 'O') {
+                    g = temp.getG() + 1;
+                } else {
+                    g = temp.getG();
+                }
+
+                int h = 0;
+
+                open.add(new DS8_AStar_Node<Point>(next, temp, g, h));
+            }
         }
+        return -1;
     }
 }
