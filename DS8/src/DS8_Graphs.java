3ffbefb737e982609f0875b5c653f779bb2000cc
dwilliam24 <k2306075@students.katyisd.org>
2026-02-06 09:44:11 -0600

    .

--- DS8/src/DS8_Graphs.java
+++ DS8/src/DS8_Graphs.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class DS8_Graphs {
+
     public static ArrayList<String> bridges(String[] edges, String vertices) {
         ArrayList<String> bridge = new ArrayList<>();
         char start = vertices.charAt(0);
@@ -9,7 +10,7 @@ public class DS8_Graphs {
             for (int edgesLoop = 0; edgesLoop <edges.length; edgesLoop++){
                 ArrayList<String> newEdges = new ArrayList<>();
                 for (int f = 0; f <edges.length; f++){
-                    if (f!=edgesLoop){
+                    if (!edges[f].equals(edges[edgesLoop])) {
                         newEdges.add(edges[f]);
                     }
                 }
@@ -22,22 +23,19 @@ public class DS8_Graphs {
                     char end =' ';
                     if (vertices.charAt(a)!=start) {
                         end = vertices.charAt(a);
+                        if (!breadthFirstSearch_Unweighted(h, vertices, start, end) && !bridge.contains(edges[edgesLoop])) {
+                            bridge.add(edges[edgesLoop]);
+                        }
                     }
-                    if(!breadthFirstSearch_Unweighted(h, vertices, start, end)&&!bridge.contains(edges[edgesLoop])){
-                        bridge.add(edges[edgesLoop]);
-                    }
-
-
-
-
-
             }
         }
             System.out.println(bridge);
             if (bridge.isEmpty()) return null;
             else return bridge;
     }
-
+    public static String[] stronglyConnectedRegions(String[] edges, String vertices) {
+        
+    }
     public static Boolean breadthFirstSearch_Unweighted(String[] edges, String vertices, char start, char end) {
         int[] indexes = new int[26];
         Arrays.fill(indexes, -1);
