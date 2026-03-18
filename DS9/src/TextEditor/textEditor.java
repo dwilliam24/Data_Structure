ecd4f502d875669553f7a130fcaf41358a72fe3c
dwilliam24 <k2306075@students.katyisd.org>
2026-03-18 10:24:15 -0500

    .

--- DS9/src/TextEditor/textEditor.java
+++ DS9/src/TextEditor/textEditor.java
@@ -6,9 +6,19 @@ import java.util.ArrayList;
 public class textEditor extends JFrame {
     private JMenuBar bar = new JMenuBar();
     private JMenu file = new JMenu("File");
+    private JMenuItem create = new JMenuItem("Create");
+    private JMenuItem open = new JMenuItem("Open");
+    private JMenuItem saveAs = new JMenuItem("Save As");
+    private JMenuItem save = new JMenuItem("Save");
+    private JMenuItem exit = new JMenuItem("Exit");
+    private JMenuItem close = new JMenuItem("Close");
     private JMenu edit = new JMenu("Edit");
+    private JMenuItem font = new JMenuItem("Font");
+    private JMenuItem replace = new JMenuItem("Replace");
+    private JMenuItem wordCount = new JMenuItem("Word Count");
     private JTabbedPane tabs = new JTabbedPane();
     private ArrayList<JPanel> JpanelTabs = new ArrayList<>();
+    private ArrayList<JTextField> text = new ArrayList<>();
 
     public textEditor(){
         super("Text Editor");
@@ -20,17 +30,26 @@ public class textEditor extends JFrame {
         bar.setDoubleBuffered(true);
         bar.add(file);
         bar.add(edit);
-        file.add("Create");
-        file.add("Open");
-        file.add("Save As");
-        file.add("Save");
-        file.add("Exit");
-        edit.add("Font");
-        edit.add("Replace");
-        edit.add("Word Count");
+        file.add(create);
+        file.add(open);
+        file.add(saveAs);
+        file.add(save);
+        file.add(exit);
+        file.add(close);
+        edit.add(font);
+        edit.add(replace);
+        edit.add(wordCount);
         JpanelTabs.add(new JPanel());
-        JpanelTabs.get(0).add(new JTextField());
-        JpanelTabs.get(0).setName("First");
+
+        for (int a=0; a<text.size(); a++){
+
+            JpanelTabs.get(a).add(text.get(a));
+            text.get(a).setBounds(10,101,10,10);
+            JpanelTabs.get(a).setName(text.get(a).getName());
+
+        }
+
+
         JpanelTabs.add(new JPanel());
         JpanelTabs.get(1).setBounds(10,10,10,10);
         JpanelTabs.get(1).setName("First");
@@ -41,6 +60,13 @@ public class textEditor extends JFrame {
         tabs.setBounds(5,10,975,425);
         add(tabs);
 
+
+
+        create.addActionListener(e-> createClicked());
         setVisible(true);
+    }
+
+    private void createClicked() {
+
     }
 }
