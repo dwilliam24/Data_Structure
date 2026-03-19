71bd30cfb7fcbc39bf23b57424c4c2929b42e646
dwilliam24 <k2306075@students.katyisd.org>
2026-03-19 10:25:24 -0500

    .

--- DS9/src/TextEditor/textEditor.java
+++ DS9/src/TextEditor/textEditor.java
@@ -1,7 +1,10 @@
 package TextEditor;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class textEditor extends JFrame {
     private JMenuBar bar = new JMenuBar();
@@ -17,15 +20,15 @@ public class textEditor extends JFrame {
     private JMenuItem replace = new JMenuItem("Replace");
     private JMenuItem wordCount = new JMenuItem("Word Count");
     private JTabbedPane tabs = new JTabbedPane();
-    private ArrayList<JPanel> JpanelTabs = new ArrayList<>();
-    private ArrayList<JTextField> text = new ArrayList<>();
+    private ArrayList<JScrollPane> scrollPanes = new ArrayList<>();
+    private ArrayList<JPanel> panels = new ArrayList<>();
+    private ArrayList<JTextArea> text = new ArrayList<>();
 
     public textEditor(){
         super("Text Editor");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(1000,500);
         setLayout(null);
-
         setJMenuBar(bar);
         bar.setDoubleBuffered(true);
         bar.add(file);
@@ -39,34 +42,64 @@ public class textEditor extends JFrame {
         edit.add(font);
         edit.add(replace);
         edit.add(wordCount);
-        JpanelTabs.add(new JPanel());
 
-        for (int a=0; a<text.size(); a++){
+        panels.add(new JPanel());
+        text.add(new JTextArea());
+        scrollPanes.add(new JScrollPane());
 
-            JpanelTabs.get(a).add(text.get(a));
-            text.get(a).setBounds(10,101,10,10);
-            JpanelTabs.get(a).setName(text.get(a).getName());
-
-        }
-
-
-        JpanelTabs.add(new JPanel());
-        JpanelTabs.get(1).setBounds(10,10,10,10);
-        JpanelTabs.get(1).setName("First");
-
-        for (JPanel jpanelTab : JpanelTabs) {
-            tabs.addTab(jpanelTab.getName(), jpanelTab);
-        }
         tabs.setBounds(5,10,975,425);
         add(tabs);
+        for (int a =0; a<panels.size(); a++){
+            scrollPanes.get(a).setBounds(10,10,200,200);
+            text.get(a).setText("dfkgjdfjgfdgkjfg");
+            scrollPanes.get(a).add(text.get(a));
+            panels.get(a).add(scrollPanes.get(a));
+            tabs.add(panels.get(a));
+        }
+        //tabs.add(new JPanel());
 
 
 
+
+
+
+        open.addActionListener(e-> openCLicked());
         create.addActionListener(e-> createClicked());
         setVisible(true);
     }
 
+    private void openCLicked() {
+        JFileChooser fileOpen = new JFileChooser("C:\\Users\\k2306075\\OneDrive - Katy Independent School District\\GitHub\\Data_Structure\\DS9\\src\\TextEditor\\Saves");
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("Text Files", "txt");
+        fileOpen.setFileFilter(filter);
+        int result = fileOpen.showOpenDialog(null);
+        if (result==JFileChooser.APPROVE_OPTION){
+            File selectedFile = fileOpen.getSelectedFile();
+            ArrayList<String> textRaw =  getFile(selectedFile);
+            text.add(new JTextArea(textRaw.toString()));
+        }
+    }
+
     private void createClicked() {
 
+
+    }
+
+    private ArrayList<String> getFile(File filePath){
+        try{
+            Scanner scanner = new Scanner(filePath);
+            ArrayList<String> list = new ArrayList<>();
+
+            while (scanner.hasNextLine()) {
+                list.add(scanner.nextLine());
+            }
+            scanner.close();
+            return list;
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            System.exit(0);
+        }
+        return null;
     }
 }
